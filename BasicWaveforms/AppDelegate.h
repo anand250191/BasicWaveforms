d53345500a9259f7183281963b3c51f73677fc52
Anand <anand.250191@gmail.com>
2014-06-29 01:38:23 +0530

    Initial Commit

---
+++ BasicWaveforms/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  BasicWaveforms
+//
+//  Created by Anand on 29/06/14.
+//  Copyright (c) 2014 Anand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ BasicWaveforms/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BasicWaveforms
+//
+//  Created by Anand on 29/06/14.
+//  Copyright (c) 2014 Anand. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
